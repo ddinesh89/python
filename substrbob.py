b3d276429f2de774c170ef3b371095b3af6c4b6c
Dinesh <dinhit15@gmail.com>
2015-01-11 22:19:05 -0800

    updating logic to use string splicing

--- substrbob.py
+++ substrbob.py
@@ -1,12 +1,15 @@
 # sample s = "bobbsoboboboboebobobobotbo"
 s = str(raw_input("Enter the input string"))
 count = 0;
-iter = 0;
+iterator = 0;
 length = len(s)
-for letter in s:
-    if (letter == "b"):
-        if (iter+1 <= length-1 and iter+2 <= length-1):
-            if (s[iter+1] == "o" and s[iter+2] == "b"):
-                count = count + 1
-    iter = iter + 1
+while (iterator < length):
+    if (iterator+3 <= length):
+        if (s[iterator:iterator+3] == "bob"):
+            count = count + 1
+            iterator = iterator + 2
+        else:
+            iterator = iterator + 1
+    else:
+        break
 print "Number of times bob occurs is: "+ str(count)
