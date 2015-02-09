deab875d0851518cd0330cccf97ddfb70e116218
ddinesh89 <dinhit15@gmail.com>
2015-02-09 10:38:23 -0800

    fixing code for returning right value when n = 6/9/20

--- countNuggets.py
+++ countNuggets.py
@@ -5,19 +5,26 @@ def McNuggets(n):
     Returns True if some integer combination of 6, 9 and 20 equals n
     Otherwise returns False.
     """
-    temp = 0
-    while(n >= 6) :
-        temp = n-6
-        if temp > 0 :
-            if temp%6 == 0 or temp%9 == 0 or temp%20 == 0 :
-                return True
-        temp = n-9
-        if temp > 0 :
-            if temp%6 == 0 or temp%9 == 0 or temp%20 == 0 :
-                return True
-        temp = n-20
-        if temp > 0 :
-            if temp%6 == 0 or temp%9 == 0 or temp%20 == 0 :
-                return True
-        n = n - 6
-    return False
+    if n == 6 :
+        return True
+    elif n == 9 :
+        return True
+    elif n == 20 :
+        return True
+    else :
+        temp = 0
+        while(n >= 6) :
+            temp = n-6
+            if temp > 0 :
+                if temp%6 == 0 or temp%9 == 0 or temp%20 == 0 :
+                    return True
+            temp = n-9
+            if temp > 0 :
+                if temp%6 == 0 or temp%9 == 0 or temp%20 == 0 :
+                    return True
+            temp = n-20
+            if temp > 0 :
+                if temp%6 == 0 or temp%9 == 0 or temp%20 == 0 :
+                    return True
+            n = n - 6
+        return False
